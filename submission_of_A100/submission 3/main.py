5b8ed793e71706c5a268359fbd6b9bea79e4ffa4
Xia Yu <1826168699@qq.com>
2023-10-27 11:25:29 +0800

    Update main.py
    sub3

--- submission_of_A100/submission 3/main.py
+++ submission_of_A100/submission 3/main.py
@@ -32,7 +32,7 @@ login(token=os.environ["HUGGINGFACE_TOKEN"])
 
 
 base_model_name ="Qwen/Qwen-14B"
-adapter_name_or_path ="xxyyy123/1698296519_Qwen14B-1025_dolly8k_cnn4kD_bbq8ks_mmlu21kRAW-r2B32LoraC-noGroup"
+adapter_name_or_path ="xxyyy123/final_1025_dolly8k_cnn4kD_bbq8ks_mmlu19kRAW_sci6k_alpaca_plus_flash_loraC_r3_bynf"
 
 model = AutoModelForCausalLM.from_pretrained(
   base_model_name,
